8cc687b448af8d79059afd1655cb781ef5a90350
Your Full Name <your.email@student.depaul.edu>
2026-02-10 18:12:02 -0600

    - created the singly linked list data structure and added logic testing in the logicTesting.py file

--- logicTesting.py
+++ logicTesting.py
@@ -1,5 +1,6 @@
 from stack import Stack
 from myqueue import Queue
+from singlyLinkedList import SinglyLinkedList
 
 def stackLogicTest():
     s = Stack()
@@ -48,10 +49,57 @@ def queueLogicTest():
     q.clear()
     print(q)
     print(q.is_empty())
+def singlyLinkedListTest():
+    lst = SinglyLinkedList()
+
+    print("is_empty:", lst.is_empty())
+    print("size:", lst.size())
+    print("pop:", lst.pop())
+    print("remove(10):", lst.remove(10))
+
+    lst.append(10)
+    lst.append(20)
+    lst.append(30)
+    print("size after appends:", lst.size())
+    print("get(0):", lst.get(0))
+    print("get(2):", lst.get(2))
+
+    lst.prepend(5)
+    lst.prepend(1)
+    print("get(0):", lst.get(0))
+    print("get(1):", lst.get(1))
+    print("size:", lst.size())
+
+    print("find(1):", lst.find(1))
+    print("find(20):", lst.find(20))
+    print("find(99):", lst.find(99))
+
+    print("remove head (1):", lst.remove(1))
+    print("remove middle (20):", lst.remove(20))
+    print("remove tail (30):", lst.remove(30))
+    print("remove missing (100):", lst.remove(100))
+
+    print("size:", lst.size())
+    print("get(0):", lst.get(0))
+    print("get(1):", lst.get(1))
+
+    print("pop:", lst.pop())
+    print("pop:", lst.pop())
+    print("pop on empty:", lst.pop())
+    print("size:", lst.size())
+
+    lst.append(99)
+    lst.append(100)
+    print("size before clear:", lst.size())
+    lst.clear()
+    print("size after clear:", lst.size())
+    print("is_empty:", lst.is_empty())
+
 
 def main():
     # stackLogicTest()
-    queueLogicTest()
+    # queueLogicTest()
+    singlyLinkedListTest()
     
 if __name__ == "__main__":
     main()
---
+++ singlyLinkedList.py
@@ -0,0 +1,134 @@
+"""
+    A singly linked list implementation using a Node class and keeping track of the head, tail, and length
+
+    Methods: 
+    is_empty()       : Return True if the Linked List is empty, False otherwise
+    size()           : Returns the length of the Linked List, 0 if empty
+    append(val)      : Adds a node with value "val" at the tail of the list
+    prepend(val)     : Adds a node with value "val" at the head of the list
+    pop()            : Removes and returns the value of the tail node
+    remove(val)      : Removes the first node containing "val" and retuns it, Returns None if not found
+    find(val)        : Returns the index of the first node containing "val", Returns "Value not found", if not found
+    get(index)       : Returns the value at the given index, Returns "Index does not exist" if out of bounds
+    clear()          : Removes all nodes from the list
+    """
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class SinglyLinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def size(self):
+        return self.length
+
+    def append(self, val):
+        new_node = Node(val)
+
+        if self.head == None: # new list case
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+    def prepend(self, val):
+        new_node = Node(val)
+
+        if self.head == None: # new list case
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+
+    def pop(self):
+        if self.head == None: # empty list case
+            return None
+        elif self.head == self.tail: # single element list case
+            val = self.head.val
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return val
+        else: 
+            current = self.head
+            while current.next != self.tail:
+                current = current.next
+            
+            val = self.tail.val
+            current.next = None
+            self.tail = current
+            self.length -= 1
+
+            return val
+
+    def remove(self, val):
+        if self.head == None: # empty list case
+            return None
+        
+        elif self.head.val == val: # removing the head case
+            removed_val = self.head.val
+            self.head = self.head.next
+            if self.head == None: # removed head was only item case
+                self.tail = None
+            self.length -= 1
+            return removed_val
+        
+        current = self.head # finding node before remove
+        while current.next and current.next.val != val:
+            current = current.next
+
+        if current.next == None: # val wasn't found
+            return None
+        else: 
+            removed_val = current.next.val
+            current.next = current.next.next
+
+            if current.next == None:
+                self.tail = current
+        self.length -= 1
+        return removed_val
+        
+
+
+    def find(self, val):
+        current = self.head
+        index = 0
+
+        while current != None:
+            if current.val == val:
+                return index
+            else:
+                current = current.next
+                index += 1
+        return "Value not found"
+    
+    def get(self, index):
+        current = self.head
+        currentIndex = 0
+
+        while current != None:
+            if currentIndex == index:
+                return current.val
+            else:
+                current = current.next
+                currentIndex += 1
+        return "Index does not exist"
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
