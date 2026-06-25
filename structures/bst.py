5c0062c8761c4a8152e929032c45460f1cd9f31f
Your Full Name <your.email@student.depaul.edu>
2026-06-25 10:34:46 -0500

    started BST shell

--- structures/bst.py
+++ structures/bst.py
@@ -1,3 +1,38 @@
 class BinarySearchTree(): 
     def __init__():
         pass
+
+    def insert():
+        pass
+
+    def delete():
+        pass 
+
+    def search():
+        pass
+    
+    def inOrder():
+        pass
+
+    def preOrder():
+        pass
+
+    def postOrder():
+        pass
+
+    def is_empty():
+        pass
+
+    def size():
+        pass
+
+    def clear():
+        pass
+
+    def findMin():
+        pass
+
+    def findMax():
+        pass
+
+    
