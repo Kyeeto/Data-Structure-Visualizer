a61adce337ae9a3fa86981b500b1104483b7e306
Your Full Name <your.email@student.depaul.edu>
2026-06-27 12:32:56 -0500

    begun work on BST

--- structures/bst.py
+++ structures/bst.py
@@ -1,38 +1,55 @@
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 class BinarySearchTree(): 
-    def __init__():
-        pass
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
-    def insert():
-        pass
+    def insert_node(self, node, val):
+        if node is None:
+            newNode = Node(val)
+            self.size +=1
+            return newNode
+        if val < node.val:
+            node.left = (self.insert_node(node.left, val))
+        else:
+            node.right = (self.insert_node(node.right, val))
+        return node
+    def insert(self, val):
+        self.root = self.insert_node(self.root, val)
 
-    def delete():
+    def delete(self):
         pass 
 
-    def search():
+    def search(self):
         pass
     
-    def inOrder():
+    def inOrder(self):
         pass
 
-    def preOrder():
+    def preOrder(self):
         pass
 
-    def postOrder():
+    def postOrder(self):
         pass
 
-    def is_empty():
+    def is_empty(self):
         pass
 
-    def size():
+    def size(self):
         pass
 
-    def clear():
+    def clear(self):
         pass
 
-    def findMin():
+    def findMin(self):
         pass
 
-    def findMax():
+    def findMax(self):
         pass
 
     
