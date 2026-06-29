2f9a163043e03d025e228d2643dc0903f7a64079
Your Full Name <your.email@student.depaul.edu>
2026-06-29 11:55:07 -0500

    bst work

--- structures/bst.py
+++ structures/bst.py
@@ -22,8 +22,33 @@ class BinarySearchTree():
     def insert(self, val):
         self.root = self.insert_node(self.root, val)
 
-    def delete(self):
-        pass 
+    def delete_node(self, node, val):
+        if node is None:
+            return None
+
+        if val < node.val:
+            node.left = self.delete_node(node.left, val)
+        elif val > node.val:
+            node.right = self.delete_node(node.right, val)
+        else:
+            if node.left is None and node.right is None:
+                self.size -= 1
+                return None
+            elif node.left is None:
+                self.size -= 1
+                return node.right
+            elif node.right is None:
+                self.size -= 1
+                return node.left
+            else:
+                successor = node.right
+                while successor.left is not None:
+                    successor = successor.left
+                node.val = successor.val
+                node.right = self.delete_node(node.right, successor.val)
+        return node
+    def delete(self, val):
+        self.root = self.delete_node(self.root, val)
 
     def search(self):
         pass
